184762754daed738638e95fbb6fc64a55f3e60dc
farizalathallah <143583446+farizalathallah@users.noreply.github.com>
2026-01-10 06:29:40 +0700

    Update edit.blade.php

--- resources/views/supplier/edit.blade.php
+++ resources/views/supplier/edit.blade.php
@@ -1,32 +1,49 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title', 'Edit Supplier')
+
+@section('content_header')
+    <h1>Edit Supplier</h1>
+@stop
 
 @section('content')
-<div class="container">
-    <div class="card">
-        <div class="card-header"><h4>Edit Supplier</h4></div>
-        <div class="card-body">
-            <form action="{{ route('supplier.update', $supplier->id) }}" method="POST">
-                @csrf
-                @method('PUT') <div class="mb-3">
-                    <label>Nama Supplier</label>
-                    <input type="text" name="nama_supplier" value="{{ $supplier->nama_supplier }}" class="form-control" required>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card card-warning">
+                <div class="card-header">
+                    <h3 class="card-title">Form Edit Data Supplier</h3>
                 </div>
-                <div class="mb-3">
-                    <label>Perusahaan</label>
-                    <input type="text" name="perusahaan" value="{{ $supplier->perusahaan }}" class="form-control" required>
-                </div>
-                <div class="mb-3">
-                    <label>Kontak</label>
-                    <input type="text" name="kontak" value="{{ $supplier->kontak }}" class="form-control" required>
-                </div>
-                <div class="mb-3">
-                    <label>Alamat</label>
-                    <textarea name="alamat" class="form-control" rows="3" required>{{ $supplier->alamat }}</textarea>
-                </div>
-                <button type="submit" class="btn btn-primary">Update</button> 
-                <a href="{{ route('supplier.index') }}" class="btn btn-secondary">Batal</a>
-            </form>
+
+                <form action="{{ route('supplier.update', $supplier->id) }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label>Nama Supplier</label>
+                            <input type="text" name="nama_supplier" value="{{ $supplier->nama_supplier }}" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Perusahaan</label>
+                            <input type="text" name="perusahaan" value="{{ $supplier->perusahaan }}" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Kontak</label>
+                            <input type="text" name="kontak" value="{{ $supplier->kontak }}" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Alamat</label>
+                            <textarea name="alamat" class="form-control" rows="3" required>{{ $supplier->alamat }}</textarea>
+                        </div>
+                    </div>
+
+                    <div class="card-footer">
+                        <a href="{{ route('supplier.index') }}" class="btn btn-secondary">Batal</a>
+                        <button type="submit" class="btn btn-primary float-right">Update</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
-@endsection
+@stop
