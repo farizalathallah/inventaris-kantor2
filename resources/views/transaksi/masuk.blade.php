10d43101da4eb7de51946a2b74baee59227aa755
farizalathallah <143583446+farizalathallah@users.noreply.github.com>
2026-01-10 06:45:01 +0700

    Update masuk.blade.php

--- resources/views/transaksi/masuk.blade.php
+++ resources/views/transaksi/masuk.blade.php
@@ -4,7 +4,7 @@
 
 @section('content_header')
     <h1><i class="fas fa-arrow-down text-success"></i> Barang Masuk</h1>
-@endsection
+@stop
 
 @section('content')
 <div class="row">
@@ -17,7 +17,7 @@
                 <div class="card-body">
                     <div class="form-group">
                         <label>Pilih Barang</label>
-                        <select name="barang_id" class="form-control select2" required>
+                        <select name="barang_id" class="form-control" required>
                             @foreach($barangs as $b)
                                 <option value="{{ $b->id }}">{{ $b->nama_barang }} (Stok: {{ $b->stok }})</option>
                             @endforeach
@@ -33,21 +33,21 @@
                     </div>
                     <div class="form-group">
                         <label>Keterangan</label>
-                        <textarea name="keterangan" class="form-control" placeholder="Contoh: Tambah stok dari Supplier A"></textarea>
+                        <textarea name="keterangan" class="form-control" placeholder="Contoh: Stok dari Supplier"></textarea>
                     </div>
                 </div>
-                <div class="card-footer text-right">
-                    <button type="submit" class="btn btn-success">Simpan Barang Masuk</button>
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-success btn-block">Simpan Barang Masuk</button>
                 </div>
             </form>
         </div>
     </div>
 
     <div class="col-md-8">
-        <div class="card">
+        <div class="card card-outline card-success">
             <div class="card-header"><h3 class="card-title">Riwayat Masuk</h3></div>
-            <div class="card-body table-responsive p-0">
-                <table class="table table-hover text-nowrap">
+            <div class="card-body p-0">
+                <table class="table table-hover">
                     <thead>
                         <tr>
                             <th>Tanggal</th>
@@ -59,7 +59,7 @@
                     <tbody>
                         @foreach($transaksis as $t)
                         <tr>
-                            <td>{{ $t->tanggal }}</td>
+                            <td>{{ \Carbon\Carbon::parse($t->tanggal)->format('d/m/Y') }}</td>
                             <td>{{ $t->barang->nama_barang }}</td>
                             <td><span class="badge badge-success">+ {{ $t->jumlah }}</span></td>
                             <td>{{ $t->keterangan }}</td>
@@ -71,4 +71,4 @@
         </div>
     </div>
 </div>
-@endsection
+@stop
