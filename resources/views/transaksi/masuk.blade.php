03edc1fb8e2d2cf705ef4f1e719cbe85bb0ac47b
farizalathallah <143583446+farizalathallah@users.noreply.github.com>
2026-01-10 08:04:35 +0700

    Update masuk.blade.php

--- resources/views/transaksi/masuk.blade.php
+++ resources/views/transaksi/masuk.blade.php
@@ -1,72 +1,39 @@
 @extends('adminlte::page')
 
-@section('title', 'Barang Masuk')
-
-@section('content_header')
-    <h1><i class="fas fa-arrow-down text-success"></i> Barang Masuk</h1>
-@stop
-
 @section('content')
-<div class="row">
-    <div class="col-md-4">
-        <div class="card card-success">
-            <div class="card-header"><h3 class="card-title">Form Input Masuk</h3></div>
-            <form action="{{ route('transaksi.store') }}" method="POST">
-                @csrf
-                <input type="hidden" name="jenis" value="masuk">
-                <div class="card-body">
-                    <div class="form-group">
-                        <label>Pilih Barang</label>
-                        <select name="barang_id" class="form-control" required>
-                            @foreach($barangs as $b)
-                                <option value="{{ $b->id }}">{{ $b->nama_barang }} (Stok: {{ $b->stok }})</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label>Jumlah</label>
-                        <input type="number" name="jumlah" class="form-control" min="1" required>
-                    </div>
-                    <div class="form-group">
-                        <label>Tanggal</label>
-                        <input type="date" name="tanggal" class="form-control" value="{{ date('Y-m-d') }}" required>
-                    </div>
-                    <div class="form-group">
-                        <label>Keterangan</label>
-                        <textarea name="keterangan" class="form-control" placeholder="Contoh: Stok dari Supplier"></textarea>
-                    </div>
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-8 col-lg-6">
+            <div class="card card-outline {{ Request::is('*masuk*') ? 'card-success' : 'card-danger' }}">
+                <div class="card-header">
+                    <h3 class="card-title">Form Input Barang {{ Request::is('*masuk*') ? 'Masuk' : 'Keluar' }}</h3>
                 </div>
-                <div class="card-footer">
-                    <button type="submit" class="btn btn-success btn-block">Simpan Barang Masuk</button>
-                </div>
-            </form>
-        </div>
-    </div>
-
-    <div class="col-md-8">
-        <div class="card card-outline card-success">
-            <div class="card-header"><h3 class="card-title">Riwayat Masuk</h3></div>
-            <div class="card-body p-0">
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>Tanggal</th>
-                            <th>Nama Barang</th>
-                            <th>Jumlah</th>
-                            <th>Keterangan</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($transaksis as $t)
-                        <tr>
-                            <td>{{ \Carbon\Carbon::parse($t->tanggal)->format('d/m/Y') }}</td>
-                            <td>{{ $t->barang->nama_barang }}</td>
-                            <td><span class="badge badge-success">+ {{ $t->jumlah }}</span></td>
-                            <td>{{ $t->keterangan }}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <form action="{{ route('transaksi.store') }}" method="POST">
+                    @csrf
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label>Pilih Barang</label>
+                            <select name="barang_id" class="form-control select2" style="width: 100%;">
+                                @foreach($barangs as $b)
+                                    <option value="{{ $b->id }}">{{ $b->nama }} (Stok: {{ $b->stok }})</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Jumlah</label>
+                            <input type="number" name="jumlah" class="form-control" required min="1">
+                        </div>
+                        <div class="form-group">
+                            <label>Tanggal</label>
+                            <input type="date" name="tanggal" class="form-control" value="{{ date('Y-m-d') }}">
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn {{ Request::is('*masuk*') ? 'btn-success' : 'btn-danger' }} btn-block">
+                            <i class="fas fa-save mr-1"></i> Simpan Transaksi
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
