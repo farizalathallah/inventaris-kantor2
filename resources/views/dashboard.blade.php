28af6c2dc7644baa5f86125bf2a143d54ec5e4e0
farizalathallah <143583446+farizalathallah@users.noreply.github.com>
2026-01-10 08:01:46 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,52 +1,34 @@
-@extends('adminlte::page')
-
-@section('title', 'Dashboard')
-
-@section('content_header')
-    <h1>Dashboard Inventaris Kantor</h1>
-@endsection
-
-@section('content')
 <div class="row">
-    <div class="col-lg-4 col-6">
+    <div class="col-lg-4 col-md-6 col-12">
         <div class="small-box bg-info">
             <div class="inner">
-                <h3>{{ number_format($totalBarang, 0, ',', '.') }}</h3>
+                <h3>533</h3>
                 <p>Total Unit Barang</p>
             </div>
-            <div class="icon">
-                <i class="fas fa-box"></i>
-            </div>
-            <a href="/barang" class="small-box-footer">Lihat Detail <i class="fas fa-arrow-circle-right"></i></a>
+            <div class="icon"><i class="fas fa-box"></i></div>
+            <a href="#" class="small-box-footer">Lihat Detail <i class="fas fa-arrow-circle-right"></i></a>
         </div>
     </div>
 
-    <div class="col-lg-4 col-6">
+    <div class="col-lg-4 col-md-6 col-12">
         <div class="small-box bg-success">
             <div class="inner">
-                <h3>Rp {{ number_format($totalAset, 0, ',', '.') }}</h3>
+                <h3>Rp 13.456</h3>
                 <p>Total Nilai Aset</p>
             </div>
-            <div class="icon">
-                <i class="fas fa-money-bill-wave"></i>
-            </div>
-            <a href="/laporan" class="small-box-footer">Lihat Laporan <i class="fas fa-arrow-circle-right"></i></a>
+            <div class="icon"><i class="fas fa-chart-line"></i></div>
+            <a href="#" class="small-box-footer">Lihat Laporan <i class="fas fa-arrow-circle-right"></i></a>
         </div>
     </div>
 
-    <div class="col-lg-4 col-6">
+    <div class="col-lg-4 col-md-6 col-12">
         <div class="small-box bg-warning">
             <div class="inner">
-                <h3 class="text-white">{{ $totalUser }}</h3>
-                <p class="text-white">User Terdaftar</p>
+                <h3>5</h3>
+                <p>User Terdaftar</p>
             </div>
-            <div class="icon">
-                <i class="fas fa-users"></i>
-            </div>
-            <a href="{{ route('users.index') }}" class="small-box-footer">
-                Info User <i class="fas fa-arrow-circle-right"></i>
-            </a>
+            <div class="icon"><i class="fas fa-users"></i></div>
+            <a href="#" class="small-box-footer">Info User <i class="fas fa-arrow-circle-right"></i></a>
         </div>
     </div>
 </div>
-@endsection
