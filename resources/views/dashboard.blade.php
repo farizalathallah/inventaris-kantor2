1196148bced3d32d35b77fba834e0d9579c5ed62
farizalathallah <143583446+farizalathallah@users.noreply.github.com>
2026-01-10 08:02:52 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,34 +1,58 @@
-<div class="row">
-    <div class="col-lg-4 col-md-6 col-12">
-        <div class="small-box bg-info">
-            <div class="inner">
-                <h3>533</h3>
-                <p>Total Unit Barang</p>
-            </div>
-            <div class="icon"><i class="fas fa-box"></i></div>
-            <a href="#" class="small-box-footer">Lihat Detail <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
+@extends('adminlte::page')
 
-    <div class="col-lg-4 col-md-6 col-12">
-        <div class="small-box bg-success">
-            <div class="inner">
-                <h3>Rp 13.456</h3>
-                <p>Total Nilai Aset</p>
-            </div>
-            <div class="icon"><i class="fas fa-chart-line"></i></div>
-            <a href="#" class="small-box-footer">Lihat Laporan <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
+@section('title', 'Dashboard')
 
-    <div class="col-lg-4 col-md-6 col-12">
-        <div class="small-box bg-warning">
-            <div class="inner">
-                <h3>5</h3>
-                <p>User Terdaftar</p>
+@section('content_header')
+    <h1>Dashboard Inventaris Kantor</h1>
+@stop
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 col-sm-6 col-lg-4">
+            <div class="small-box bg-info shadow-sm">
+                <div class="inner">
+                    <h3>533</h3>
+                    <p>Total Unit Barang</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-box"></i>
+                </div>
+                <a href="{{ route('barang.index') }}" class="small-box-footer">
+                    Lihat Detail <i class="fas fa-arrow-circle-right"></i>
+                </a>
             </div>
-            <div class="icon"><i class="fas fa-users"></i></div>
-            <a href="#" class="small-box-footer">Info User <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+
+        <div class="col-12 col-sm-6 col-lg-4">
+            <div class="small-box bg-success shadow-sm">
+                <div class="inner">
+                    <h3>Rp 13.456</h3>
+                    <p>Total Nilai Aset</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-money-bill-wave"></i>
+                </div>
+                <a href="{{ route('laporan.index') }}" class="small-box-footer">
+                    Lihat Laporan <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-lg-4">
+            <div class="small-box bg-warning shadow-sm">
+                <div class="inner">
+                    <h3>5</h3>
+                    <p>User Terdaftar</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-users"></i>
+                </div>
+                <a href="{{ route('users.index') }}" class="small-box-footer">
+                    Info User <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
         </div>
     </div>
 </div>
+@stop
