39d724e6b6267295981346f3d866e2df45c08036
farizalathallah <143583446+farizalathallah@users.noreply.github.com>
2026-01-10 13:29:47 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard Inventaris')
+@section('title', 'Dashboard')
 
 @section('content_header')
     <h1>Dashboard Inventaris Kantor</h1>
@@ -9,62 +9,38 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-        <div class="col-12 col-sm-6 col-lg-4">
-            <div class="small-box bg-info shadow-sm">
+        <div class="col-lg-4 col-6">
+            <div class="small-box bg-info shadow">
                 <div class="inner">
-                    <h3>{{ number_format($totalBarang) }}</h3>
+                    <h3>{{ number_format($totalBarang ?? 0) }}</h3>
                     <p>Total Unit Barang</p>
                 </div>
-                <div class="icon">
-                    <i class="fas fa-box"></i>
-                </div>
-                <a href="{{ route('barang.index') }}" class="small-box-footer">
-                    Lihat Detail <i class="fas fa-arrow-circle-right"></i>
-                </a>
+                <div class="icon"><i class="fas fa-box"></i></div>
+                <a href="{{ route('barang.index') }}" class="small-box-footer">Lihat Detail <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
 
-        <div class="col-12 col-sm-6 col-lg-4">
-            <div class="small-box bg-success shadow-sm">
+        <div class="col-lg-4 col-6">
+            <div class="small-box bg-success shadow">
                 <div class="inner">
-                    {{-- Format Rupiah Otomatis --}}
-                    <h3 style="white-space: nowrap;">Rp {{ number_format($totalAset, 0, ',', '.') }}</h3>
+                    <h3 style="white-space: nowrap;">Rp {{ number_format($totalAset ?? 0, 0, ',', '.') }}</h3>
                     <p>Total Nilai Aset</p>
                 </div>
-                <div class="icon">
-                    <i class="fas fa-money-bill-wave"></i>
-                </div>
-                <a href="{{ route('transaksi.laporan') }}" class="small-box-footer">
-                    Lihat Laporan <i class="fas fa-arrow-circle-right"></i>
-                </a>
+                <div class="icon"><i class="fas fa-money-bill-wave"></i></div>
+                <a href="{{ route('transaksi.laporan') }}" class="small-box-footer">Lihat Laporan <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
 
-        <div class="col-12 col-sm-6 col-lg-4">
-            <div class="small-box bg-warning shadow-sm">
+        <div class="col-lg-4 col-12">
+            <div class="small-box bg-warning shadow">
                 <div class="inner">
-                    <h3>{{ $totalUser }}</h3>
+                    <h3>{{ $totalUser ?? 0 }}</h3>
                     <p>User Terdaftar</p>
                 </div>
-                <div class="icon">
-                    <i class="fas fa-users"></i>
-                </div>
-                <a href="{{ route('users.index') }}" class="small-box-footer">
-                    Info User <i class="fas fa-arrow-circle-right"></i>
-                </a>
+                <div class="icon"><i class="fas fa-users"></i></div>
+                <a href="{{ route('users.index') }}" class="small-box-footer">Info User <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
     </div>
 </div>
-@stop
-
-@section('css')
-<style>
-    /* Agar tampilan tetap rapi di layar HP */
-    @media (max-width: 576px) {
-        .small-box h3 {
-            font-size: 1.4rem !important;
-        }
-    }
-</style>
 @stop
